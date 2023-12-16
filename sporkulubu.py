58e929b990c012c07e3304797c361b8e0ef11871
Enes Ceylan <63819815+MedusaCollins@users.noreply.github.com>
2023-12-16 18:07:41 +0300

    Refactor code to use rich library for console printing

--- sporkulubu.py
+++ sporkulubu.py
@@ -10,6 +10,7 @@
 
 from rich.console import Console
 from rich.table import Table
+from rich import print
 
 spor_kulubu = {
     "üye" : {"erkek": ["enes","ufuk","ulaş","burak","caner"], "kadın": ["ipek","özden", "nihal"]},
@@ -40,27 +41,25 @@ for branş, detaylar in spor_kulubu["branş"].items():
     sertifika = "Var" if detaylar['sertifika'] else "Yok"
     branş_rows.append([branş.capitalize(), yaş, cinsiyet.capitalize(), sertifika])
 
-
-
-green, red = "\033[32m", "\033[31m"
-console = Console()
-console.print(tablo_olustur("Kayıt olması gerekenler", ["Cinsiyet", "İsimler"], üye_rows), 
+Console().print(tablo_olustur("Kayıt olması gerekenler", ["Cinsiyet", "İsimler"], üye_rows), 
               tablo_olustur("Branşlar", ["Branş", "Yaş", "Cinsiyet", "Sertifika Zorunluluğu"], branş_rows), 
               justify="center")
+
+
 
 isim, yas, brans, sertifika = [item.strip() for item in input("Lütfen sırasıyla isim, yaş, branş ve sertifikanızın olup olmadığını(e/h) aralarına virgül bırakarak yazınız: ").split(',')]
 
 if isim not in spor_kulubu["üye"]["erkek"] and isim not in spor_kulubu["üye"]["kadın"]:
-    print(f"{red}İsminiz listede bulunmadığı için kaydınızı gerçekleştiremeyiz.")
+    print("[red]İsminiz listede bulunmadığı için kaydınızı gerçekleştiremeyiz.")
 else:
     if brans not in spor_kulubu["branş"]:
-        print(f"{red}Girdiğiniz branş bulunmamaktadır.")
+        print("[red]Girdiğiniz branş bulunmamaktadır.")
     else:
         if int(yas) < spor_kulubu["branş"][brans]["yaş"][0] or int(yas) > spor_kulubu["branş"][brans]["yaş"][1]:
-            print(f"{red}{brans} kulübüne yaşınızın ötürü katılamazsınız.")
+            print(f"[red]{brans} kulübüne yaşınızın ötürü katılamazsınız.")
         else:
             if isim in spor_kulubu["üye"]["erkek"] and "erkek" not in spor_kulubu["branş"][brans]["cinsiyet"] and isim in spor_kulubu["üye"]["kadın"] and "kadın" not in spor_kulubu["branş"][brans]["cinsiyet"]:
-                print(f"{red}{brans} kulübe cinsiyetinizin ötürü katılamazsınız.")
+                print(f"[red]{brans} kulübe cinsiyetinizin ötürü katılamazsınız.")
             else:
                 if sertifika.upper() == "E" and spor_kulubu["branş"][brans]["sertifika"] == True or spor_kulubu["branş"][brans]["sertifika"] == False:
-                    print(f"{green}{isim.capitalize()}, {brans} kulübüne kaydınız başarıyla gerçekleşmiştir.")
+                    print(f"[green]{isim.capitalize()}, {brans} kulübüne kaydınız başarıyla gerçekleşmiştir.")
