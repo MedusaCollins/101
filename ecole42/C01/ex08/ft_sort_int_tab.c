d2e30dc7b9ba715e08996968a0c33ede45d1da65
Enes Ceylan <enesfurkanceylan@hotmail.com>
2024-08-18 14:58:47 +0300

    Added C01 examples.

---
+++ ecole42/C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eceylan <eceylan@student.42istanbul.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 14:51:33 by eceylan           #+#    #+#             */
+/*   Updated: 2024/08/18 14:54:14 by eceylan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				swap = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = swap;
+			}
+			i++;
+		}
+		size--;
+	}
+}
