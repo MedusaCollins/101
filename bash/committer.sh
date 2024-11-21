f6f8f4ac809a8c74199aa5bdef2c643d86d85211
Enes Ceylan <contact@enesceylan.dev>
2024-11-21 07:52:18 +0300

    Updated commit script to stop spesific date.

--- bash/committer.sh
+++ bash/committer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-start_date="2024-11-06"
+start_date="2024-11-20"
+end_date="2024-11-20"
 
-file_name="example.txt"
-
-days_to_commit=$((($(date +%s) - $(date -d "$start_date" +%s)) / 86400))
+days_to_commit=$((($(date -d "$end_date" +%s) - $(date -d "$start_date" +%s)) / 86400))
 
 min_commits=1
 max_commits=5
+
+file_name="example.txt"
 
 current_date=$(date -d "$start_date" +%Y-%m-%d)
 
@@ -18,7 +19,6 @@ for ((i = 0; i <= $days_to_commit; i++)); do
     echo "Random text $(date +%s%N)" >>"$file_name"
 
     git add "$file_name"
-
     GIT_AUTHOR_DATE="$current_date 12:00:00" GIT_COMMITTER_DATE="$current_date 12:00:00" \
       git commit -m "Commit from $current_date with random change $j"
   done
